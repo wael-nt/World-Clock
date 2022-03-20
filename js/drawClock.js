aaf89e0ecff14a4ffb0dce0499196a29ffb40973
Wael <wael.natafji0003@stud.hkr.se>
2022-03-20 22:22:18 +0100

    Add js script to draw the clocks with style sheet , Still not functional

---
+++ js/drawClock.js
@@ -0,0 +1,40 @@
+async function showTime(){
+    let date = await getTimeFromCity();
+    let h = date.getHours(); // 0 - 23
+    let m = date.getMinutes(); // 0 - 59
+    let s = date.getSeconds(); // 0 - 59
+    let session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    let time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+}
+setInterval(async () => {
+  d = await getTimeFromCity();; //object of date depends on which city is selected
+  hr = d.getHours();
+  min = d.getMinutes();
+  sec = d.getSeconds();
+  hr_rotation = 30 * hr + min / 2; // converting current time
+  min_rotation = 6 * min;
+  sec_rotation = 6 * sec;
+
+  hour.style.transform = `rotate(${hr_rotation}deg)`;
+  minute.style.transform = `rotate(${min_rotation}deg)`;
+  second.style.transform = `rotate(${sec_rotation}deg)`;
+}, 1000);
+
+setInterval(showTime, 1000);
+
